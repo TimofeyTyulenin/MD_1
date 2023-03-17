45ab3528cc4df9edba14de16d5b29d9f6b482386
TimofeyTyulenin <tiulenko36@gmail.com>
2023-03-18 00:36:21 +0700

    3

---
+++ app/src/main/java/com/example/md_1/onboarding/slides/SlideFragment2.kt
@@ -0,0 +1,38 @@
+package com.example.md_1.onboarding.slides
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
+import com.example.md_1.R
+
+
+class SlideFragment2 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_slide2, container, false)
+
+        val viewPager = activity?.findViewById<ViewPager2>(R.id.viewPager)
+
+        view.findViewById<Button>(R.id.button2).setOnClickListener {
+            viewPager?.currentItem = 2
+        }
+
+        view.findViewById<TextView>(R.id.txtskip).setOnClickListener{
+            findNavController().navigate(R.id.action_viewPagerFragment_to_loginActivity)
+        }
+       return view
+    }
+
+
+}
