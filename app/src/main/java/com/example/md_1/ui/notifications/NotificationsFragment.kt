863498305510e6d55e8b7a37afca8c0a8da775f9
TimofeyTyulenin <tiulenko36@gmail.com>
2023-03-28 20:44:40 +0700

    6

--- app/src/main/java/com/example/md_1/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/md_1/ui/notifications/NotificationsFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,15 +31,25 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        binding.wv.webViewClient =MyWebViewClient()
+        binding.wv.settings.javaScriptEnabled = true
+        binding.wv.loadUrl("https://learnenglish.britishcouncil.org/general-english/video-zone")
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+}
+
+class MyWebViewClient: WebViewClient(){
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        if( !( request?.url.toString().startsWith( "https://learnenglish.britishcouncil.org/general-english/video-zone" ) ) ) {
+            return true
+        }
+
+        return super.shouldOverrideUrlLoading(view, request)
     }
 }
